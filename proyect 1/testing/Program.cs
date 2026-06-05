c0c7ed37cf284239b74cb9bd9c5e499c772420be
kastalco <kastalcowork@gmail.com>
2026-06-05 07:28:03 -0500

    new gitignore and other files created

--- proyect 1/testing/Program.cs
+++ proyect 1/testing/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, Platzi!");
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+var connectionString = config["ConnectionStrings:DefaultConnection"] ?? "Cadena no encontrada";
+
+Console.WriteLine("Hello, Platzi!");
+Console.WriteLine($"Cadena de conexión simulada: {connectionString}");
